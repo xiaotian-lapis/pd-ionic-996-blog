284cbda7f0e0881557e3f06ab5cb0d90b4f40626
xiaotian-lapis <xiaotian.li@lapisit.com.au>
2024-01-30 10:39:49 +1100

    feat(my-blog-app): add amplify

--- apps/my-blog-app/capacitor.config.ts
+++ apps/my-blog-app/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'org.workers996.blogapp',
   appName: 'My 996 blog app',
-  webDir: '../../dist/apps/my-blog-app/browser',
+  webDir: './dist/apps/my-blog-app/browser',
   server: {
     androidScheme: 'https'
   }
