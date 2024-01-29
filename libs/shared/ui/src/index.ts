04457b1d7aa3770e7998337859da45485bb2df6d
xiaotian-lapis <xiaotian.li@lapisit.com.au>
2024-01-29 16:13:58 +1100

    added shared-ui

---
+++ libs/shared/ui/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/shared-ui/shared-ui.component';
+export * from './lib/shared-ui/footer/footer.component';
+export * from './lib/shared-ui/header/header.component';
+export * from './lib/shared-ui/map-icon/marker-icon.resource';
